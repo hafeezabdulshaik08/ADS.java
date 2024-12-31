18afe82317df8933f708cce9cb7c266dd180767f
SHAIK ABDUL HAFEEZ <168513325+hafeezabdulshaik08@users.noreply.github.com>
2024-12-31 10:41:31 +0530

    Update 01 lab.java

--- 01 lab.java
+++ 01 lab.java
@@ -118,5 +118,62 @@ public class Insertionsort {
 
 
 
+import java.util.Scanner;
+public class binarysearch
+{
+	public static int bs(int a[],int key,int n)
+	{
+		int low = 0;
+		int high = n-1;
+		int mid;
+		
+		while(low <= high)
+		{
+			mid = (low+high)/2;
+			if (key == a[mid])
+			{
+				return mid;
+			}
+			else if (key < a[mid])
+			{
+				high = mid-1;
+			
+			}
+			else 
+			{
+				low = mid+1;
+			}
+		}
+		return -1;
+		
+	}	
+	public static void main(String[] args)
+	{
+	Scanner sc = new Scanner(System.in);
+	int i,j,n,key;
+	System.out.println("enter the number of elements in the array ");
+	n = sc.nextInt();
+	int a[] =  new int [n];
+	System.out.println("enter the elements of the array");
+	for(i = 0;i<n;i++)
+	{
+		j = sc.nextInt();
+		a[i] = j;
+	
+	}
+	System.out.println("enter the key");
+	key = sc.nextInt();
+	
+	
+	int result = bs(a,key,n);
+	if(result == -1)
+	{
+		System.out.println("the key is not found");
+	}
+	else
+	{
+		System.out.println("the key is found at the index"+result);
+	}
+}
 
 
