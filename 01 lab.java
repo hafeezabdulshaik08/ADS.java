3609f2f1404cfb59b2bb955b4bd3490d4d8d8b4e
SHAIK ABDUL HAFEEZ <168513325+hafeezabdulshaik08@users.noreply.github.com>
2024-12-31 10:24:03 +0530

    Update 01 lab.java

--- 01 lab.java
+++ 01 lab.java
@@ -41,3 +41,44 @@
 
 }
 
+
+
+import java.util.Scanner;
+public class selectionsort{
+	public static void Selectionsort(int a[],int n){
+		int i,j,minpos,temp;
+		for(i=0;i<n-1;i++){
+			minpos=i;
+			for(j=i+1;j<=n-1;j++){
+				if(a[j]<a[minpos])
+					minpos=j;
+			}
+			temp=a[i];
+			a[i]=a[minpos];
+			a[minpos]=temp;
+		}
+	}
+	public static void main(String[] args){
+        	int n,i,j;
+        	Scanner sc = new Scanner(System.in);
+        	System.out.println("enter the number of elements of the array");
+        	n = sc.nextInt();
+        	int a[] = new int[n];
+
+        	System.out.println("enter the elements");
+       		for(i = 0;i<n;i++){
+            	j = sc.nextInt();
+            	a[i] = j;
+        	}
+        	Selectionsort(a,n);
+        	for(i = 0; i<n;i++){
+            		System.out.print("  "+a[i] );
+        	}
+    	}
+
+
+}
+
+
+
+
